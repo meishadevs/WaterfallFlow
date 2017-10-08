427dd91896d35843fc6dda77257d1d9c50fe9609
meishadevs <meishadevs@163.com>
2017-10-08 15:55:12 +0800

    update some data

--- js/app.js
+++ js/app.js
@@ -6,47 +6,49 @@
  */
 
 //整个html页面加载完成后,调用此方法
-window.onload = function()
-{
+window.onload = function () {
+
     //设置图片的CS样式
     setImageCss("container", "box");
 }
 
+
 //监听滚动条，当滚动滚动条后,调用此方法
-window.onscroll = function()
-{
+window.onscroll = function () {
+
     //加载图片
     loadImage();
 }
 
+
 //加载图片
-function loadImage()
-{
+function loadImage() {
+
     //创建一个json字符串,用于保存待加载的图片信息
     var imgData = {
         "data": [
-            { "src": "1.jpg" },
-            { "src": "2.jpg" },
-            { "src": "3.jpg" },
-            { "src": "4.jpg" },
-            { "src": "5.jpg" },
-            { "src": "6.jpg" },
-            { "src": "7.jpg" },
-            { "src": "8.jpg" },
-            { "src": "9.jpg" },
-            { "src": "10.jpg" }
+            {"src": "1.jpg"},
+            {"src": "2.jpg"},
+            {"src": "3.jpg"},
+            {"src": "4.jpg"},
+            {"src": "5.jpg"},
+            {"src": "6.jpg"},
+            {"src": "7.jpg"},
+            {"src": "8.jpg"},
+            {"src": "9.jpg"},
+            {"src": "10.jpg"}
         ]
     };
 
     //如果允许加载图片
-    if (checkFlag())
-    {
+    if (checkFlag()) {
+
         //获得container
         var container = document.getElementById("container");
 
         //遍历json字符串中的数据
-        for (var i = 0; i < imgData.data.length; i++)
-        {
+        for (var i = 0; i < imgData.data.length; i++) {
+
             //创建一个box
             var box = document.createElement("div");
 
@@ -69,7 +71,7 @@ function loadImage()
             var img = document.createElement("img");
 
             //设置img的src属性
-            img.src = "image/" + imgData.data[i].src;
+            img.src = "images/" + imgData.data[i].src;
 
             //将img添加到boxing中
             boximg.appendChild(img);
@@ -80,9 +82,10 @@ function loadImage()
     }
 }
 
+
 //设置图片的css样式
-function setImageCss(parent, content)
-{
+function setImageCss(parent, content) {
+
     //获得id选择器名称为container的div标签
     var container = document.getElementById(parent);
 
@@ -99,20 +102,19 @@ function setImageCss(parent, content)
     //设置container的css样式
     container.style.cssText = "width:" + boxWidth * num + "px;" + "margin:0 auto;";
 
-    //创建数组,保存box的高度
+    //创建数组,保存每个box的高度
     var boxHeightArr = [];
 
     //遍历所有的box
-    for(var i = 0; i < box.length; i++)
-    {
+    for (var i = 0; i < box.length; i++) {
+
         //将第一行box中，box的高度保存在boxHeightArr数组中
-        if(i < num)
-        {
+        if (i < num) {
             boxHeightArr[i] = box[i].offsetHeight;
         }
         //处理第二行box
-        else
-        {
+        else {
+
             //获得数组boxHeightArr中最小的数值
             //获得第一行box中，高度最小的box值
             var minHeight = Math.min.apply(null, boxHeightArr);
@@ -135,21 +137,27 @@ function setImageCss(parent, content)
     }
 }
 
-//获得父标签下的类选择器名称为className的子标签
-function getChildElement(parent, className)
-{
-    //创建数组,保存父级标签下的类选择器名称为className的子标签
+
+/**
+ * 获得父标签下的类选择器名称为className的子标签
+ * @param parent 父标签
+ * @param className 父标签下的子标签的类名
+ * @returns {Array} 子标签数组
+ */
+function getChildElement(parent, className) {
+
+    //创建数组,用于保存父级标签下的类选择器名称为className的子标签
     var contentArr = [];
 
     //获得parent标签下的所有子标签
     var allcontent = parent.getElementsByTagName("*");
 
     //遍历子标签
-    for(var i = 0; i < allcontent.length; i++)
-    {
+    for (var i = 0; i < allcontent.length; i++) {
+
         //如果标签的类选择器名称为className;
-        if(allcontent[i].className == className)
-        {
+        if (allcontent[i].className == className) {
+
             //将标签添加到contentArr数组中
             contentArr.push(allcontent[i]);
         }
@@ -158,21 +166,20 @@ function getChildElement(parent, className)
     return contentArr;
 }
 
+
 //获得minHeight在数组BoxHeightArr中的下标
-function getMinHeightIndex(BoxHeightArr, minHeight)
-{
-    for(var i in BoxHeightArr)
-    {
-        if(BoxHeightArr[i] == minHeight)
-        {
+function getMinHeightIndex(BoxHeightArr, minHeight) {
+    for (var i in BoxHeightArr) {
+        if (BoxHeightArr[i] == minHeight) {
             return i;
         }
     }
 }
 
+
 //检测是否允许加载数据
-function checkFlag()
-{
+function checkFlag() {
+
     //获得container
     var container = document.getElementById("container");
 
@@ -188,18 +195,13 @@ function checkFlag()
     //获得当前页面的高度
     var pageHeight = document.documentElement.clientHeight || document.body.clientHeight;
 
-    //console.log("滚动条到窗口顶部的距离:" + parseInt(scollTop));
-    //console.log("当前页面的高度:" + parseFloat(pageHeight));
-    //console.log("最后一个box到顶部的距离:" + parseInt(lastBoxHeight));
-    //console.log("*******************************");
+    if (lastBoxHeight < scollTop + pageHeight) {
 
-    if (lastBoxHeight < scollTop + pageHeight)
-    {
         //允许加载图片
         return true;
     }
-    else
-    {
+    else {
+
         //不允许加载图片
         return false;
     }
